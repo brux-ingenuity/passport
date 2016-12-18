bb2c79732728098776c334f4c92dec5b8fe977f4
Brux Romuar <brux@ingenuity.ph>
2016-12-19 01:55:04 +0800

    Disable throttling for now.

--- src/RouteRegistrar.php
+++ src/RouteRegistrar.php
@@ -69,7 +69,7 @@ class RouteRegistrar
     {
         $this->router->post('/oauth/token', [
             'uses' => 'AccessTokenController@issueToken',
-            'middleware' => 'throttle'
+            //'middleware' => 'throttle'
         ]);
 
         $this->router->group(['middleware' => ['web', 'auth']], function ($router) {
